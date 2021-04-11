eb56631b0b1af79c8053e3c815694a303638f80a
JeanCarlosSC <jeancarlosodi@gmail.com>
2021-04-11 12:44:11 -0500

    first

---
+++ listas.py
@@ -0,0 +1,29 @@
+pila = []
+
+pila.append(1)
+pila.append(2)
+pila.append(3)
+
+print("Mi pila")
+print(pila)
+
+print(pila.pop())
+print(pila.pop())
+print(pila.pop())
+
+print(pila)
+
+pila.append(1)
+pila.append(2)
+pila.append(3)
+
+print("Mi lista")
+print(pila)
+
+print(pila.pop(0))
+print(pila.pop(0))
+print(pila.pop(0))
+
+print(pila)
+
+
