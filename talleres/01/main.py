655fe467ed0a75d8da748177fa9ca1b339501c5b
JeanCarlosSC <jeancarlosodi@gmail.com>
2021-04-17 20:59:51 -0500

    updated

--- talleres/01/main.py
+++ talleres/01/main.py
@@ -1,5 +1,6 @@
 class Pila:
     """ Representa una pila con operaciones de apilar, desapilar y verificar si está vacía. """
+
     def __init__(self):
         """ Crea una pila vacía. """
         # La pila vacía se representa con una lista vacía
@@ -18,48 +19,53 @@ class Pila:
         except IndexError:
             raise ValueError("La pila está vacía")
 
+    @property
     def es_vacia(self):
         """ Devuelve True si la lista está vacía, False si no. """
         return self.items == []
 
+
 class Nodo:
-    def __init__(self, valor, izquierda=None, derecha=None):
-        self.valor = valor
+    def __init__(self, value, izquierda=None, derecha=None):
+        self.valor = value
         self.izquierda = izquierda
         self.derecha = derecha
 
-def en_orden(arbol):
-    if arbol == None:
-        return ''
-    elif arbol.valor in ['+', '-', '*', '/']:
-        return "(" + en_orden(arbol.izquierda) + str(arbol.valor) + en_orden(
-            arbol.derecha) + ")"
-    else:
-        return str(arbol.valor)
 
-def evaluar(arbol):
-    if arbol == None:
+def en_orden(tree):
+    if tree is None:
+        return ''
+    elif tree.valor in ['+', '-', '*', '/']:
+        return "(" + en_orden(tree.izquierda) + str(tree.valor) + en_orden(
+            tree.derecha) + ")"
+    else:
+        return str(tree.valor)
+
+
+def evaluar(tree):
+    if tree is None:
         pass
     else:
-        if arbol.valor == '+':
-            return evaluar(arbol.izquierda) + evaluar(arbol.derecha)
-        if arbol.valor == '-':
-            return evaluar(arbol.izquierda) - evaluar(arbol.derecha)
-        if arbol.valor == '*':
-            return evaluar(arbol.izquierda) * evaluar(arbol.derecha)
-        if arbol.valor == '/':
-            return evaluar(arbol.izquierda) // evaluar(arbol.derecha)
-        return int(arbol.valor)
+        if tree.valor == '+':
+            return evaluar(tree.izquierda) + evaluar(tree.derecha)
+        if tree.valor == '-':
+            return evaluar(tree.izquierda) - evaluar(tree.derecha)
+        if tree.valor == '*':
+            return evaluar(tree.izquierda) * evaluar(tree.derecha)
+        if tree.valor == '/':
+            return evaluar(tree.izquierda) // evaluar(tree.derecha)
+        return int(tree.valor)
+
 
 # Guardamos cada línea en la pila
 f = open("operaciones.txt", "r")
 pila = Pila()
 for linea in f:
-  pila.apilar(linea[:-1])
+    pila.apilar(linea[:-1])
 f.close()
 
 # por cada linea se convierte a un árbol y se muestra la información procesada
-while (not pila.es_vacia()):
+while not pila.es_vacia:
     linea = pila.desapilar()
     inicio = 0
     valores = Pila()
@@ -67,20 +73,20 @@ while (not pila.es_vacia()):
         valor = ""
         # Tokeniza en nodos
         if linea[i] == " ":
-          valor = linea[inicio:i]
-          valores.apilar(Nodo(valor))
-          inicio = i + 1
+            valor = linea[inicio:i]
+            valores.apilar(Nodo(valor))
+            inicio = i + 1
         elif i == len(linea) - 1:
-          valor = linea[inicio:i + 1]
-          valores.apilar(Nodo(valor))
+            valor = linea[inicio:i + 1]
+            valores.apilar(Nodo(valor))
         # Organiza los nodos
-        if (valor in ['+', '-', '*', '/']):
-          operador=valores.desapilar().valor  # Desapilar símbolo
-          b = valores.desapilar() # Desapila valor b
-          a = valores.desapilar() # Desapila valor a
-          nodo = Nodo(operador, a, b) # Guarda todo en un nodo
-          valores.apilar(nodo) # Apila el nodo resultante
+        if valor in ['+', '-', '*', '/']:
+            operador = valores.desapilar().valor  # Desapilar símbolo
+            b = valores.desapilar()  # Desapila valor b
+            a = valores.desapilar()  # Desapila valor a
+            nodo = Nodo(operador, a, b)  # Guarda todo en un nodo
+            valores.apilar(nodo)  # Apila el nodo resultante
     # Muestra el ultimo valor resultante
     arbol = valores.desapilar()
     print("Nueva línea de operaciones:")
-    print(linea+" = "+str(en_orden(arbol))+" = "+str(evaluar(arbol)))
+    print(linea + " = " + str(en_orden(arbol)) + " = " + str(evaluar(arbol)))
